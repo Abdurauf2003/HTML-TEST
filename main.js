ce6424558bf1eb80a58cacaf5384cd5a6ac58c36
Abdurauf Egamov <abuegamov03@gmail.com>
2023-02-08 12:10:04 +0500

    Little changes are applied! 08.02.2023

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// Function to display the time
+function displayTime() {
+    // Create a new Date object
+    const date = new Date();
+    // Get the hours, minutes, and seconds from the date object
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    // Display the time in the format "hh:mm:ss"
+    const time = `${hours}:${minutes}:${seconds}`;
+    // Update the time on the web page
+    document.getElementById("time").innerHTML = time;
+}
+  // Call the displayTime function every 1000 milliseconds (1 second)
+  setInterval(displayTime, 1000);  
+
 function tekshir() {
     var a = document.getElementById("l0");
     var c = document.getElementById("l1");
